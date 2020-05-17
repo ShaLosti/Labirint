92fcd88c8a712eba0725be487642ee0883b8a4e5
ShaLosti <neyw444@gmail.com>
2020-05-17 12:59:16 +0300

    Create project

---
+++ Labirint/Assets/Scripts/PlrController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlrController : MonoBehaviour
+{
+    [SerializeField] private float _walkSpeed = 3f;
+    [SerializeField] private float _runSpeed = 7f;
+    private float _moveSpeed = 3f;
+
+    private Rigidbody2D _rb;
+
+    private PlrInput _plrInput = new PlrInput();
+    void Start()
+    {
+        _moveSpeed = _walkSpeed;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        _plrInput.GetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(_rb.position + _plrInput.movementDirection * _moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Labirint/Assets/Scripts/PlrInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlrInput
+{
+    public Vector2 movementDirection;
+
+    public void GetInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+}
