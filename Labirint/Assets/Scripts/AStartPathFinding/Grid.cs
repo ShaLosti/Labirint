68d2ca2fe6a6600423cd4ac640047d497364ccef
ShaLosti <neyw444@gmail.com>
2020-05-20 17:26:10 +0300

    Add simple Path-finding to the monster

---
+++ Labirint/Assets/Scripts/AStartPathFinding/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask unWalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y,1));
+    }
+}
