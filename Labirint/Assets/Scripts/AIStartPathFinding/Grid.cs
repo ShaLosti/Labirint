fe212f242dde3d43f3e4deabf2d2edfac8058b79
ShaLosti <neyw444@gmail.com>
2020-05-23 16:37:50 +0300

    Modify path-finding script

--- Labirint/Assets/Scripts/AIStartPathFinding/Grid.cs
+++ Labirint/Assets/Scripts/AIStartPathFinding/Grid.cs
@@ -1,67 +1,102 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System;
-/*If you are making it for 2D, use Physics2D.OverlapCircle isntead of checkSphere
-Since this is still the best video on pathfinding I'm hijacking 
-top comment to say that all you need to do to make it 2D after using 
-Physics2D.OverlapCircle is to change your two Vector3.forwards to Vector3.up 
-and in the next video change worldPosition.z to worldPosition.y.
-Oh and change your Gizmos.DrawWireCube to: 
-Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));*/
+
 namespace RootNamespace.AIStartPathFinding
-{ 
-    public class Grid : MonoBehaviour
-    {
-        public LayerMask unWalkableMask;
-        public Vector2 gridWorldSize;
-        public float nodeRadius;
-        Node[,] grid;
+{
+	public class Grid : MonoBehaviour
+	{
+		//public Transform plr;
+		public bool displayGrid;
+		public LayerMask unwalkableMask;
+		public Vector2 gridWorldSize;
+		public float nodeRadius;
+		Node[,] grid;
 
-        private float nodeDiameter;
-        int gridSizeX, gridSizeY;
+		float nodeDiameter;
+		int gridSizeX, gridSizeY;
 
-        private void Start()
-        {
-            nodeDiameter = nodeRadius*2;
-            gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
-            gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+		void Awake()
+		{
+			nodeDiameter = nodeRadius * 2;
+			gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+			gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+			CreateGrid();
+		}
 
-            CreateGrid();
-        }
+		public int maxSize
+		{
+			get
+			{
+				return gridSizeX * gridSizeY;
+			}
+		}
 
-        private void CreateGrid()
-        {
-            grid = new Node[gridSizeX,gridSizeY];
+		void CreateGrid()
+		{
+			grid = new Node[gridSizeX, gridSizeY];
+			Vector2 worldBottomLeft = new Vector2(transform.position.x, transform.position.y) - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+			for (int x = 0; x < gridSizeX; x++)
+			{
+				for (int y = 0; y < gridSizeY; y++)
+				{
+					Vector2 worldPoint = worldBottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+					bool walkable = (Physics2D.OverlapCircle(worldPoint, nodeRadius/*-.01f*/, unwalkableMask) == null);
+					grid[x, y] = new Node(walkable, worldPoint,x,y);
+				}
+			}
+		}
 
-            Vector2 worldBottomLeft = new Vector2(transform.position.x,transform.position.y) 
-                - Vector2.right * gridWorldSize.x / 2
-                - Vector2.up * gridWorldSize.y;
+		public List<Node> GetNeighbourse(Node node)
+		{
+			List<Node> neighbourse = new List<Node>();
 
-            for (int x = 0; x < gridSizeX; x++)
-            {
-                for (int y = 0; y < gridSizeY; y++)
-                {
-                    Vector2 worldPoint = worldBottomLeft + Vector2.right
-                        * (x * nodeDiameter + nodeRadius) + Vector2.up 
-                        * (y * nodeDiameter + nodeRadius);
-                    bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, unWalkableMask));
-                    grid[x, y] = new Node(walkable, worldPoint);
-                }
-            }
-        }
+			for (int x = -1; x <= 1; x++)
+			{
+				for (int y = -1; y <= 1; y++)
+				{
+					if (x == 0 && y == 0) 
+						continue;
 
-        private void OnDrawGizmos()
-        {
-            Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
-            if(grid != null)
-            {
-                foreach (Node node in grid)
-                {
-                    Gizmos.color = (node.walkable) ? Color.white : Color.red;
-                    Gizmos.DrawCube(node.worldPosition, Vector2.one * (nodeDiameter - 0.1f));
-                }
-            }
-        }
-    }
+					int checkX = node.gridX + x;
+					int checkY = node.gridY + y;
+
+					if(checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+					{	
+						neighbourse.Add(grid[checkX,checkY]);
+					}
+				}
+			}
+			return neighbourse;
+		}
+
+		public Node NodeFromWorldPoint(Vector2 worldPosition)
+		{
+			float percentX = worldPosition.x / gridWorldSize.x + 0.5f;
+			float percentY = worldPosition.y / gridWorldSize.y + 0.5f;
+			//float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+			//float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+
+			percentX = Mathf.Clamp01(percentX);
+			percentY = Mathf.Clamp01(percentY);
+
+			int x = Mathf.FloorToInt(Mathf.Min(gridSizeX * percentX, gridSizeX - 1));
+			int y = Mathf.FloorToInt(Mathf.Min(gridSizeY * percentY, gridSizeY - 1));
+			return grid[x, y];
+		}
+
+		public List<Node> path;
+		void OnDrawGizmos()
+		{
+			Gizmos.DrawWireCube(transform.position, new Vector2(gridWorldSize.x, gridWorldSize.y));
+			if (grid != null && displayGrid)
+			{
+				foreach (Node n in grid)
+				{
+					Gizmos.color = (n.walkable) ? Color.white : Color.red;
+					Gizmos.DrawWireCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+				}
+			}
+		}
+	}
 }
---
+++ Labirint/Assets/Scripts/AIStartPathFinding/Pathfinding.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RootNamespace.AIStartPathFinding
+{
+    public class Pathfinding : MonoBehaviour
+    {
+        PathRequestManager requestManager;
+
+        Grid grid;
+
+        private void Awake()
+        {
+            TryGetComponent<PathRequestManager>(out requestManager);
+            TryGetComponent<Grid>(out grid);
+        }
+        public void StartFindPath(Vector2 startPos, Vector2 targetPos)
+        {
+            StartCoroutine(FindPath(startPos, targetPos));
+        }
+        IEnumerator FindPath(Vector2 startPos, Vector2 targetPos)
+        {
+            Vector2[] wayPoints = new Vector2[0];
+            bool pathSuccess = false;
+
+            Node startNode = grid.NodeFromWorldPoint(startPos);
+            Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+            if (startNode.walkable && targetNode.walkable)
+            {
+
+                Heap<Node> openSet = new Heap<Node>(grid.maxSize);
+                HashSet<Node> closeSet = new HashSet<Node>();
+
+                openSet.Add(startNode);
+
+                while (openSet.Count > 0)
+                {
+                    Node currentNode = openSet.RemoveFirst();
+
+                    closeSet.Add(currentNode);
+
+                    if (currentNode == targetNode)
+                    {
+                        pathSuccess = true;
+                        break;
+                    }
+
+                    foreach (Node neighbour in grid.GetNeighbourse(currentNode))
+                    {
+                        if (!neighbour.walkable || closeSet.Contains(neighbour))
+                            continue;
+
+                        int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                        if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                        {
+                            neighbour.gCost = newMovementCostToNeighbour;
+                            neighbour.hCost = GetDistance(neighbour, targetNode);
+                            neighbour.parent = currentNode;
+
+                            if (!openSet.Contains(neighbour))
+                                openSet.Add(neighbour);
+
+                        }
+                    }
+                }
+            }
+            yield return null;
+            if (pathSuccess)
+            {
+                wayPoints = RetracePath(startNode, targetNode);
+            }
+            requestManager.FinishProcessingPath(wayPoints, pathSuccess);
+
+        }
+
+        Vector2[] RetracePath(Node startNode, Node endNode)
+        {
+            List<Node> path = new List<Node>();
+
+            Node currentNode = endNode;
+
+            while(currentNode != startNode)
+            {
+                if (currentNode == startNode)
+                    path.Add(currentNode);
+                currentNode = currentNode.parent;
+            }
+            Vector2[] waypoints = SimplifyPath(path);
+            Array.Reverse(waypoints);
+            return waypoints;
+        }
+
+        Vector2[] SimplifyPath(List<Node> path)
+        {
+            List<Vector2> waypoints = new List<Vector2>();
+            Vector2 directionOld = Vector2.zero;
+
+            for (int i = 1; i < path.Count; i++)
+            {
+                Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+                if (directionNew != directionOld)
+                {
+                    waypoints.Add(path[i - 1].worldPosition);
+                }
+                directionOld = directionNew;
+            }
+            return waypoints.ToArray();
+        }
+
+        int GetDistance (Node nodeA, Node nodeB)
+        {
+            int DistX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+            int DistY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+            //if (DistX > DistY)
+            //    return 14 * DistY + 10 * (DistX - DistY);
+
+            //return 14 * DistX + 10 * (DistY - DistX);
+            return Mathf.Abs(nodeA.gridX - nodeB.gridX) + Mathf.Abs(nodeA.gridY - nodeB.gridY);
+        }
+    }
+}
