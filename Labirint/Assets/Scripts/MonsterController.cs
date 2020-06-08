e14a8821ec04405eaf8370fcc536e7e2c40f0458
ShaLosti <neyw444@gmail.com>
2020-06-08 13:13:37 +0300

    Add TimeLine

--- Labirint/Assets/Scripts/MonsterController.cs
+++ Labirint/Assets/Scripts/MonsterController.cs
@@ -7,14 +7,17 @@ public class MonsterController : MonoBehaviour
 {
     private NavMeshAgent agent;
 
+    [SerializeField] public GameObject targetObject;
+
     private void Start()
     {
         TryGetComponent<NavMeshAgent>(out agent);
+        targetObject = FindObjectOfType<CharacterInput>().gameObject;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
     private void Update()
     {
-        agent.SetDestination(FindObjectOfType<CharacterInput>().transform.position);
+        agent.SetDestination(targetObject.transform.position);
     }
 }
